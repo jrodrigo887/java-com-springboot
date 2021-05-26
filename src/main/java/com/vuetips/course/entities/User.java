ab5381c53ef226f7bd501239fa340d5eb5439e2e
joao Rodrigo <joao.filipe@vsoft.com.br>
2021-05-25 22:18:26 -0300

    H2 database, test profile, JPA

--- src/main/java/com/vuetips/course/entities/User.java
+++ src/main/java/com/vuetips/course/entities/User.java
@@ -2,22 +2,30 @@ package com.vuetips.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_user")
 public class User implements Serializable {
-	
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
-	public User() {
-		
+	public User() {		
 	}
 
 	public User(long id, String name, String email, String phone, String password) {
