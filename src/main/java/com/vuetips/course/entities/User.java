155dda8750201d276dec3a8ddc63f78f5037d035
jrodrigo887 <jrodrigo887@gmail.com>
2021-05-26 22:50:27 -0300

    1° Order, Instant, ISO 8601

--- src/main/java/com/vuetips/course/entities/User.java
+++ src/main/java/com/vuetips/course/entities/User.java
@@ -1,11 +1,14 @@
 package com.vuetips.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -26,6 +29,13 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 	
 	public User() {		
 	}
